8f9c144a6aeb4c0e1530138d15b8b34e480343ad
Lucas Donato <lucaspolimig96@gmail.com>
2019-05-14 21:11:34 -0300

    ajustando ninjaflix

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,3 +8,11 @@ Before("@login") do
   @login_page.go
   @login_page.with("tony@stark.com", "123456")
 end
+
+After do |scenario|
+  # if scenario.failed?
+  shot_file = page.save_screenshot("log/screenshot.png")
+  shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+  embed(shot_b64, "image/png", "Screenshot") # Cucumber anexa o screenshot no report
+  # end
+end
