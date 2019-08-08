9351d7c10dfdc531b25a0148bdec2121163b24ed
=lucasdonato <=lucaspolimig96@gmail.com>
2019-08-08 09:56:03 -0300

    alterando tempo do token

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -2,11 +2,11 @@
 
 module Helpers
   def get_token
-    10.times do
+    20.times do
       js_script = 'return window.localStorage.getItem("default_auth_token");'
       @token = page.execute_script(js_script)
       break if @token != nil
-      sleep 10
+      sleep 20
     end
     @token
   end
